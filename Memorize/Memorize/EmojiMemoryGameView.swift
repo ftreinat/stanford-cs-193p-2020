5a89c36fb315112ef0abf117f7f2175f81aab2a4
Florian Treinat <ftreinat>
2020-06-20 21:22:22 +0200

    Lecture 6 done

--- Memorize/Memorize/EmojiMemoryGameView.swift
+++ Memorize/Memorize/EmojiMemoryGameView.swift
@@ -15,7 +15,9 @@ struct EmojiMemoryGameView: View {
         VStack() {
              Grid(items: viewModel.cards) { card in
                 CardView(card: card).onTapGesture {
-                    self.viewModel.choose(card: card)
+                    withAnimation(.linear(duration: 0.75)) {
+                        self.viewModel.choose(card: card)
+                    }
                 }
                 .padding(5)
             }
@@ -27,7 +29,10 @@ struct EmojiMemoryGameView: View {
                 Text("Points: \(viewModel.points)")
             }.padding()
             Button("Start Game") {
+                withAnimation(.easeInOut) {
                 self.viewModel.startNewGame()
+                }
+
             }
                 .buttonStyle(DefaultButtonStyle())
                 .padding()
@@ -68,26 +73,58 @@ struct CardView: View {
 //                    .padding(5).opacity(0.4)
 //                Text(card.content)
 //                    .font(Font.system(size: fontSize(for: size)))
+//                   .rotationEffect(Angle.degrees(card.isMatched ? 360 : 0))
+//                   .animation(card.isMatched ? Animation.linear(duration: 1).repeatForever(autoreverses: false) : .default)
 //            }
 //                .cardify(isFaceUp: card.isFaceUp)
+//                .transition(AnyTransition.scale)
 //        }
 //    }
 
+    @State private var animatedBonusRemaining: Double = 0
+    
+    private func startBonusTimeAnimation() {
+        animatedBonusRemaining = card.bonusRemaining
+        withAnimation(.linear(duration: card.bonusTimeRemaining)) {
+            animatedBonusRemaining = 0
+        }
+    }
+    
+    // poor mans Logging inside SwiftUI Views, da alle Statements eigentlich Views sein müssen 
+    func printData() -> some View {
+        print("card \(card) mit AnimatedBonus: \(animatedBonusRemaining) und bonusRemaining \(card.bonusRemaining)")
+        return EmptyView()
+    }
     
     private func body(for size: CGSize) -> some View {
         Group {
             if card.isFaceUp || !card.isMatched {
                 ZStack {
-                    Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(110-90), clockwise: true)
+                    Group {
+                        printData()
+                        if card.isConsumingBonusTime {
+                            Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(-animatedBonusRemaining*360-90), clockwise: true)
+                                .onAppear {
+                                    self.startBonusTimeAnimation()
+                                }
+                        } else {
+                            Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(-card.bonusRemaining*360-90), clockwise: true)
+                            
+                        }
+                    }
                         .padding(5).opacity(0.4)
+                    .transition(.identity)
                     Text(card.content)
                         .font(Font.system(size: fontSize(for: size)))
+                        .rotationEffect(Angle.degrees(card.isMatched ? 360 : 0))
+                        .animation(card.isMatched ? Animation.linear(duration: 1).repeatForever(autoreverses: false) : .default)
                 }
                     .cardify(isFaceUp: card.isFaceUp)
+                    
             } else {
                  EmptyView()
             }
-        }
+        }.transition(AnyTransition.scale)
     }
 
     private func fontSize(for size: CGSize) -> CGFloat {
