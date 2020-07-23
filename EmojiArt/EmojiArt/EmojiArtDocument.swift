21e9c1cf882748ffa3a5e47db498285fb25c15b6
Florian Treinat <ftreinat>
2020-07-23 16:01:26 +0200

    Kurz vor Image laden

--- EmojiArt/EmojiArt/EmojiArtDocument.swift
+++ EmojiArt/EmojiArt/EmojiArtDocument.swift
@@ -9,5 +9,41 @@
 import SwiftUI
 
 class EmojiArtDocument : ObservableObject {
+        
+    static let palette: String = "🏈🏄‍♂️🚴‍♂️🥌🛁🦋"
     
+    @Published private var emojiArt: EmojiArt = EmojiArt()
+    
+    @Published private(set) var backgroundImage: UIImage?
+    
+    // MARK: - Intent(s)
+    
+    func addEmoji(_ emoji: String, at location: CGPoint, size: CGFloat) {
+        emojiArt.addEmoji(emoji, x: Int(location.x), y: Int(location.y), size: Int(size))
+    }
+    
+    func moveEmoji(_ emoji: EmojiArt.Emoji, by offset: CGSize) {
+        if let index = emojiArt.emojis.firstIndex(matching: emoji) {
+            emojiArt.emojis[index].x += Int(offset.width)
+            emojiArt.emojis[index].y += Int(offset.height)
+        }
+    }
+    
+    func scaleEmoji(_ emoji: EmojiArt.Emoji, by scale: CGFloat) {
+        if let index = emojiArt.emojis.firstIndex(matching: emoji) {
+            emojiArt.emojis[index].size = Int((CGFloat(emojiArt.emojis[index].size) * scale).rounded(.toNearestOrEven))
+        }
+    }
+    
+    func setBackgroundURL(_ url: URL?) {
+        emojiArt.backgroundURL = url?.imageURL
+        fetchBackgroundImageData()
+    }
+    
+    private func fetchBackgroundImageData() {
+        backgroundImage = nil
+        if let url = self.emojiArt.backgroundURL {
+            
+        }
+    }
 }
--- EmojiArt/EmojiArt/EmojiArtDocumentView.swift
+++ EmojiArt/EmojiArt/EmojiArtDocumentView.swift
@@ -13,8 +13,41 @@ struct EmojiArtDocumentView: View {
     @ObservedObject var document: EmojiArtDocument
     
     var body: some View {
-        Text("Hello, World!")
+//        \.self ist ein Keypath und gibt ein Attribut eines Objektes an. Hier self
+        VStack {
+            ScrollView(.horizontal) {
+                HStack {
+                    ForEach(EmojiArtDocument.palette.map { String($0) }, id: \.self) { emoji in
+                        Text(emoji)
+                            .font(Font.system(size: self.defaultEmojiSize))
+                    }
+                }
+            }
+            .padding(.horizontal)
+            
+            Color.white
+            .overlay(getBackgroundImageWithCompatability())
+// Klappt leider beim alten XCode nicht -> iOS Target 13.2 unter Mojave. Umweg mittels func getBackgroundImageAsView
+//                .overlay(
+//                    Group {
+//                        if self.document.backgroundImage != nil {
+//                            Image(self.document.backgroundImage!)
+//                        }
+//                    }
+//            )
+                .edgesIgnoringSafeArea([.horizontal, .bottom])
+        }
     }
+    
+    @ViewBuilder
+    func getBackgroundImageWithCompatability() -> some View {
+        // Annotation ViewBuilder bewirkt, dass im else Fall kein EmptyView angegeben werden muss
+        if document.backgroundImage != nil {
+            return Image(uiImage: document.backgroundImage!)
+        }
+    }
+    
+    private let defaultEmojiSize: CGFloat = 40
 }
 
 struct ContentView_Previews: PreviewProvider {
